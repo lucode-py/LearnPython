085b042d32fe1e19593a8717ae307f493c743501
Lucas Dugard <lucas.dugard@icloud.com>
2023-07-03 18:08:47 +0200

    play update for windows: vous pourrez relencez les script avec un boutton dans la sidebar cette fois ci sur windows.

--- windows/gui_edit.py
+++ windows/gui_edit.py
@@ -1,6 +1,7 @@
 from customtkinter import *
 import subprocess
-
+import psutil
+import signal
 
 window = CTk()
 window.geometry("550x900")
@@ -15,6 +16,20 @@ with open('sortie.txt', 'w') as fichier_sortie:
 def change_appearance_mode_event(new_appearance_mode: str):
     set_appearance_mode(new_appearance_mode)
 
+def reboot():
+    # Recherche du processus par nom
+    nom_processus = "python.exe"
+
+    # Parcourir les processus et trouver l'ID du processus spécifique
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.name() == nom_processus:
+            id_processus = proc.pid
+            print(type(id_processus))
+            break
+
+    os.kill(id_processus, signal.SIGTERM)
+    with open('sortie.txt', 'w') as fichier_sortie:
+        subprocess.Popen(['python', "playground_windows.py"], stdout=fichier_sortie, stderr=subprocess.STDOUT)
 
 # sidebar
 sidebar_frame = CTkFrame(window, width=140, corner_radius=0)
@@ -25,10 +40,7 @@ sidebar_frame.grid_rowconfigure(4, weight=1)
 logo_label = CTkLabel(sidebar_frame, text="CustomTkinter", font=CTkFont(size=20, weight="bold"))
 logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-sidebar_button_1 = CTkButton(sidebar_frame)
-sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-
-play_button = CTkButton(sidebar_frame, text="Executer le programe")
+play_button = CTkButton(sidebar_frame, text="Executer le programe", command=reboot)
 play_button.grid(row=6, column=0, padx=20, pady=(10, 10))
 
 appearance_mode_optionemenu = CTkOptionMenu(sidebar_frame, values=["Dark", "Light", "System"], command=change_appearance_mode_event)
@@ -36,7 +48,7 @@ appearance_mode_optionemenu.grid(row=11, column=0, padx=20, pady=(10, 10))
 
 
 
-box_cour = CTkTextbox(window, width=330, font=("Cascadia Mono SemiBold", 15))
+box_cour = CTkTextbox(window, width=330, font=("menlo", 15))
 box_cour.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 box_cour.insert("5.5", "edit your code")
 
